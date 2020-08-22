d1557434407482d4919f639dea1b1d476bddf4e8
German Solano <kontrolg1239@gmail.com>
2020-08-22 17:43:31 -0400

    Remove createAt and updateAt columns from role

--- api/migrations/20200822212319-create-role.js
+++ api/migrations/20200822212319-create-role.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('roles', {
+    await queryInterface.createTable("roles", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,18 +10,10 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('roles');
+    await queryInterface.dropTable("roles");
   }
 };
