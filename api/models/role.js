9754c7ab0ec0a5a245251f2245d480c536f5b5fb
German Solano <kontrolg1239@gmail.com>
2020-08-23 09:17:23 -0400

    Add undescore naming option to role model

--- api/models/role.js
+++ api/models/role.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class role extends Model {
     /**
@@ -12,12 +10,16 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  role.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'role',
-  });
+  }
+  role.init(
+    {
+      name: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: "role",
+      underscored: true
+    }
+  );
   return role;
 };
